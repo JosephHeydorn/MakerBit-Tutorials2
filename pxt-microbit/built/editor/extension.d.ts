3b29b2e0c4d5ab01d79dcee63655340f592b24d5
Joseph Heydorn <joey.heydorn@yahoo.com>
2019-10-01 15:51:34 -0700

    File Add

---
+++ pxt-microbit/built/editor/extension.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="../../node_modules/pxt-core/localtypings/pxtarget.d.ts" />
+/// <reference path="../../node_modules/pxt-core/built/pxtblocks.d.ts" />
+/// <reference path="../../node_modules/pxt-core/built/pxtcompiler.d.ts" />
+/// <reference path="../../node_modules/pxt-core/built/pxtlib.d.ts" />
+/// <reference path="../../node_modules/pxt-core/built/pxteditor.d.ts" />
+/// <reference path="../../editor/dapjs.d.ts" />
+export declare const bufferConcat: (bufs: Uint8Array[]) => Uint8Array;
